89fcaf7e728edec5276c2b67ee44b7441639bfff
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-01-22 11:06:54 +0700

    Add project files.

---
+++ RunGroupWebApp/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroupWebApp.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Street { get; set; }
+
+        public String City { get; set; }
+        public String State { get; set; }
+
+    }
+}
